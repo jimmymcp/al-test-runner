cd6a5a64666373a3b18af1617268af0c65dbc8a4
James Pearson <james.pearson@tecman.co.uk>
2019-11-15 14:09:00 +0000

    Error when credentials not entered

--- PowerShell/Get-ALTestRunnerCredential.ps1
+++ PowerShell/Get-ALTestRunnerCredential.ps1
@@ -4,6 +4,9 @@ function Get-ALTestRunnerCredential {
 
     if (($UserName -eq '') -or ($SecurePwd -eq '')) {
         $Credential = Get-Credential -UserName $UserName -Message ('Please enter the credentials to connect to server {0}' -f (Get-ServerFromLaunchJson))
+        if ($null -eq $Credential) {
+            throw "Credentials not entered"
+        }
         Set-ALTestRunnerCredential -Credential $Credential
         return $Credential
     }
