5aed5e8e3952e3e807a5384588de98e344fcba3d
James Pearson <james.pearson@tecman.co.uk>
2020-02-03 17:39:24 +0000

    Bug fix pssession collection and creation

--- PowerShell/Get-DockerHostSession.ps1
+++ PowerShell/Get-DockerHostSession.ps1
@@ -1,12 +1,12 @@
 function Get-DockerHostSession {
-    $Session = Get-PSSession -ComputerName (Get-ValueFromALTestRunnerConfig -KeyName dockerHost) -State Opened
+    $Session = Get-PSSession | Where-Object ComputerName -eq (Get-ValueFromALTestRunnerConfig -keyName 'dockerHost') | Where-Object State -eq Opened
     if ($null -eq $Session) {
 
         $Script = "New-PSSession -ComputerName $(Get-ValueFromALTestRunnerConfig -KeyName 'dockerHost') "
         
         if (!([string]::IsNullOrEmpty((Get-ValueFromALTestRunnerConfig -KeyName vmUserName)))) {
-            $Script = '$Credential =' + "[pscredential]::new($(Get-ValueFromALTestRunnerConfig -KeyName vmUserName), $(Get-ValueFromALTestRunnerConfig -KeyName vmSecurePassword));" + $Script
-            $Script += '-Credential = $Credential '
+            $Script = '$Credential=' + "[pscredential]::new('$(Get-ValueFromALTestRunnerConfig -KeyName vmUserName)', (ConvertTo-SecureString $(Get-ValueFromALTestRunnerConfig -KeyName vmSecurePassword)));" + $Script
+            $Script += '-Credential $Credential '
         }
         
 
