fd32acfd84d1d58188897b5ca78b08aef2530fc5
James Pearson <james.pearson@tecman.co.uk>
2020-07-23 11:21:12 +0100

    Handle comments in launch.json

--- PowerShell/Get-ValueFromLaunchJson.ps1
+++ PowerShell/Get-ValueFromLaunchJson.ps1
@@ -8,8 +8,28 @@ function Get-ValueFromLaunchJson {
         $KeyName
     )
     
-    $LaunchJson = ConvertFrom-Json (Get-Content -Path $LaunchJsonPath -Raw)
+    $LaunchJson = Get-LaunchJson -Path $LaunchJsonPath
     ($LaunchJson.configurations | Where-Object name -eq $ConfigName).$KeyName
 }
 
+function Get-LaunchJson {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Path
+    )
+
+    $LaunchJson = Get-Content $Path
+    $LaunchJson = $LaunchJson | Where-Object {$_.ToString().Trim().StartsWith('//') -eq $false}
+    $LaunchJson = $LaunchJson -join [Environment]::NewLine
+
+    while (($LaunchJson.IndexOf('/*') -gt 0) -and ($LaunchJson.IndexOf('*/') -gt 0)) {
+        $Start = $LaunchJson.IndexOf('/*')
+        $End = $LaunchJson.IndexOf('*/')
+        $LaunchJson = $LaunchJson.Remove($Start, $End - $Start + 2)
+    }
+
+    ConvertFrom-Json $LaunchJson
+}
+
 Export-ModuleMember -Function Get-ValueFromLaunchJson
+Export-ModuleMember -Function Get-LaunchJson
---
+++ PowerShell/Tests/Get-ValueFromLaunchJson.Tests.ps1
@@ -0,0 +1,57 @@
+Describe Get-LaunchJson {
+    Context "launch.json has no comments" {
+        It "create the PowerShell object from the JSON correctly" {
+            $LaunchPath = Join-Path $TestDrive 'launch.json'
+            Set-Content -Path $LaunchPath `
+                -Value '{"version": "0.2.0", "configurations": [{"name": "configone", "server": "serverone"},{"name": "configtwo", "server": "servertwo"}]}'
+            
+            Get-LaunchJson -Path $LaunchPath
+        }
+    }
+
+    Context "launch.json has line comments" {
+        It "create the PowerShell object from the JSON correctly" {
+            $LaunchPath = Join-Path $TestDrive 'launch.json'
+            Set-Content -Path $LaunchPath `
+                -Value `
+                '{
+                    "version": "0.2.0",
+                    "configurations": [
+                        //{
+                        //    "name": "configone",
+                        //    "server": "serverone"
+                        //},
+                        {
+                            "name": "configtwo",
+                            "server": "servertwo"
+                        }
+                    ]
+                }'
+            
+            (Get-LaunchJson -Path $LaunchPath).configurations.Length | should -be 1
+        }
+    }
+
+    Context "launch.json has block comments" {
+        It "create the PowerShell object from the JSON correctly" {
+            $LaunchPath = Join-Path $TestDrive 'launch.json'
+            Set-Content -Path $LaunchPath `
+                -Value `
+                '{
+                    "version": "0.2.0",
+                    "configurations": [
+                        /*{
+                            "name": "configone",
+                            "server": "serverone"
+                        },*/
+                        {
+                            "name": "configtwo",
+                            "server": "servertwo"
+                        }
+                    ]
+                }'
+            
+            (Get-LaunchJson -Path $LaunchPath).configurations.Length | should -be 1
+        }
+    }
+}
