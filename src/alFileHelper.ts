48b7be4bc0ff2741caa575cd9e7d3a8c8d23264f
James Pearson <james.pearson@norriq.com>
2021-02-03 13:23:19 +0000

    Move file methods into alFileHelper module

--- src/alFileHelper.ts
+++ src/alFileHelper.ts
@@ -1,5 +1,7 @@
 import * as vscode from 'vscode';
 import { ALObject } from './types';
+import { getCurrentWorkspaceConfig } from './extension';
+import { readFileSync} from 'fs';
 
 export function getALObjectOfDocument(document: vscode.TextDocument): ALObject | undefined {
     let documentText = document.getText(new vscode.Range(0, 0, 1, 0));
@@ -14,4 +16,54 @@ export function getALObjectOfDocument(document: vscode.TextDocument): ALObject |
         return ALObject;
     }
     return undefined;
+}
+
+export function documentIsTestCodeunit(document: vscode.TextDocument): boolean {
+	if (document.fileName.substr(document.fileName.lastIndexOf('.')) !== '.al') {
+		return false;
+	}
+
+	const text = document.getText(new vscode.Range(0, 0, 10, 0));
+	return (text.match('Subtype = Test;') !== null);
+}
+
+export function getDocumentIdAndName(document: vscode.TextDocument): string {
+	let firstLine = document.getText(new vscode.Range(0, 0, 0, 250));
+	let matches = firstLine.match('\\d+ .*');
+	if (matches !== undefined) {
+		return matches!.shift()!.replace(/"/g, '');
+	}
+	else {
+		return '';
+	}
+}
+
+
+export async function getFilePathByCodeunitId(codeunitId: number, method?: string): Promise<string> {
+	return new Promise(async (resolve, reject) => {
+		const config = getCurrentWorkspaceConfig();
+		const globPattern = config.testCodeunitGlobPattern;
+		if ((globPattern === '') || (globPattern === undefined)) {
+			resolve('');
+		}
+
+		const files = await vscode.workspace.findFiles(globPattern);
+		for (let file of files) {
+			const text = readFileSync(file.fsPath, { encoding: 'utf-8' });
+			if (text.startsWith('codeunit ' + codeunitId)) {
+				let filePath = file.fsPath;
+				if (method !== undefined) {
+					const matches = text.match('procedure.*' + method + '\(\)');
+					if (matches !== null) {
+						const document = await vscode.workspace.openTextDocument(file.fsPath);
+						const lineNo = document.positionAt(matches!.index!).line + 1;
+						filePath += ':' + lineNo;
+					}
+				}
+				resolve(filePath);
+			}
+		}
+
+		resolve('could not find codeunit ' + codeunitId + ' with pattern ' + globPattern);
+	});
 }
--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,7 @@ import * as xml2js from 'xml2js';
 import * as types from './types';
 import { CodelensProvider } from './CodelensProvider';
 import { updateCodeCoverageDecoration } from './CodeCoverage';
+import { documentIsTestCodeunit, getDocumentIdAndName, getFilePathByCodeunitId} from './alFileHelper';
 
 let terminal: vscode.Terminal;
 export let activeEditor = vscode.window.activeTextEditor;
@@ -526,26 +527,6 @@ async function selectLaunchConfig() {
 	setALTestRunnerConfig('launchConfigName', selectedConfig);
 }
 
-export function documentIsTestCodeunit(document: vscode.TextDocument): boolean {
-	if (document.fileName.substr(document.fileName.lastIndexOf('.')) !== '.al') {
-		return false;
-	}
-
-	const text = document.getText(new vscode.Range(0, 0, 10, 0));
-	return (text.match('Subtype = Test;') !== null);
-}
-
-export function getDocumentIdAndName(document: vscode.TextDocument): string {
-	let firstLine = document.getText(new vscode.Range(0, 0, 0, 250));
-	let matches = firstLine.match('\\d+ .*');
-	if (matches !== undefined) {
-		return matches!.shift()!.replace(/"/g, '');
-	}
-	else {
-		return '';
-	}
-}
-
 export function getRangeOfFailingLineFromCallstack(callstack: string, method: string, document: vscode.TextDocument): vscode.Range | void {
 	const methodStartLineForCallstack = getLineNumberOfMethodDeclaration(method, document);
 	if (methodStartLineForCallstack === -1) {
@@ -568,35 +549,6 @@ export function getLineNumberOfMethodDeclaration(method: string, document: vscod
 	}
 
 	return document.positionAt(match!.index!).line;
-}
-
-export async function getFilePathByCodeunitId(codeunitId: number, method?: string): Promise<string> {
-	return new Promise(async (resolve, reject) => {
-		const config = getCurrentWorkspaceConfig();
-		const globPattern = config.testCodeunitGlobPattern;
-		if ((globPattern === '') || (globPattern === undefined)) {
-			resolve('');
-		}
-
-		const files = await vscode.workspace.findFiles(globPattern);
-		for (let file of files) {
-			const text = readFileSync(file.fsPath, { encoding: 'utf-8' });
-			if (text.startsWith('codeunit ' + codeunitId)) {
-				let filePath = file.fsPath;
-				if (method !== undefined) {
-					const matches = text.match('procedure.*' + method + '\(\)');
-					if (matches !== null) {
-						const document = await vscode.workspace.openTextDocument(file.fsPath);
-						const lineNo = document.positionAt(matches!.index!).line + 1;
-						filePath += ':' + lineNo;
-					}
-				}
-				resolve(filePath);
-			}
-		}
-
-		resolve('could not find codeunit ' + codeunitId + ' with pattern ' + globPattern);
-	});
 }
 
 export function getCodeunitIdFromAssemblyName(assemblyName: string): number {
@@ -747,7 +699,7 @@ export function getWorkspaceFolder() {
 	throw new Error('Please open a file in the project you want to run the tests for.');
 }
 
-function getCurrentWorkspaceConfig() {
+export function getCurrentWorkspaceConfig() {
 	return vscode.workspace.getConfiguration('al-test-runner', vscode.Uri.file(getWorkspaceFolder()));
 }
 
