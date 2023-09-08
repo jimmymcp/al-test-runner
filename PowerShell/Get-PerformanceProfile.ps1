4fea28df893d844a23240e8850fd6985f1dece09
James Pearson <james.pearson@aptean.com>
2023-09-08 10:22:15 +0100

    Warning message when unable to download performance profile

--- PowerShell/Get-PerformanceProfile.ps1
+++ PowerShell/Get-PerformanceProfile.ps1
@@ -16,9 +16,15 @@ function Get-PerformanceProfile {
         Method      = 'Post'
         ContentType = 'application/json'
     }
+    
     $Result = (Invoke-InvokeWebRequest $Params).Content | ConvertFrom-Json
-    $PerformanceProfile = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Result.value))
-    Set-Content -Path $PerformanceProfilePath -Value $PerformanceProfile
+    if ($null -ne $Result) {
+        $PerformanceProfile = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Result.value))
+        Set-Content -Path $PerformanceProfilePath -Value $PerformanceProfile
+    }
+    else {
+        Write-Host "Could not download performance profile. Please ensure at least v1.4 of the Test Runner Service app is installed." -ForegroundColor DarkRed
+    }
 }
 
 Export-ModuleMember -Function Get-PerformanceProfile
