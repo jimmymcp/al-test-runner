c509ac6faa431874f17cf7ca60711ccff0ed1593
Johannes Wikman <jwikman@users.noreply.github.com>
2024-02-14 11:41:40 +0100

    Improved error handling

--- PowerShell/Invoke-InvokeWebRequest.ps1
+++ PowerShell/Invoke-InvokeWebRequest.ps1
@@ -11,12 +11,17 @@ function Invoke-InvokeWebRequest {
         Invoke-WebRequest @Params
     }
     catch {
+        $errorObject = $_
         try {
             $ErrorDetails = ConvertFrom-Json $_.ErrorDetails
             Write-Host $ErrorDetails.error.message -ForegroundColor DarkRed
         }
         catch {
-            Write-Host $_ -ForegroundColor DarkRed
+            # Probably not a JSON formatted error
+            Write-Host $errorObject -ForegroundColor DarkRed
+            if ($errorObject.ErrorDetails.Message -match 'Unable to read data from the transport connection') {
+                Write-Host "Are you trying to connect to a SSL port with `"http://`" instead of `"https://`"? (Uri: $($Params.Uri))" -ForegroundColor DarkRed
+            }
         }
     }
 }
