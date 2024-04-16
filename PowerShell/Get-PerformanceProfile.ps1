156a01229931c6a3335ecfd39267199dd6e84485
James Pearson <james.pearson@totalenterprisesolutions.co.uk>
2024-04-16 11:35:50 +0100

    [BC24] Downloading Performance Profile or Code Coverage results in a timeout Fixes #134

--- PowerShell/Get-PerformanceProfile.ps1
+++ PowerShell/Get-PerformanceProfile.ps1
@@ -10,23 +10,45 @@ function Get-PerformanceProfile {
     $PerformanceProfilePath = './.altestrunner/PerformanceProfile.alcpuprofile'
 
     Write-Host "Downloading performance profile to $PerformanceProfilePath"
-    $Params = @{
-        Uri         = $ServiceUrl
-        Credential  = $Credential
-        Method      = 'Post'
-        ContentType = 'application/json'
-    }
 
-    $responseData = (Invoke-InvokeWebRequest $Params).Content
-    if ($responseData) {
-        $Result = $responseData | ConvertFrom-Json
-    }
-    if ($null -ne $Result) {
-        $PerformanceProfile = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Result.value))
-        Set-Content -Path $PerformanceProfilePath -Value $PerformanceProfile
+    if (Get-UrlIsForOData $ServiceUrl) {
+        $Params = @{
+            Uri         = $ServiceUrl
+            Credential  = $Credential
+            Method      = 'Post'
+            ContentType = 'application/json'
+        }
+
+        $responseData = (Invoke-InvokeWebRequest $Params).Content
+        if ($responseData) {
+            $Result = $responseData | ConvertFrom-Json
+        }
+        if ($null -ne $Result) {
+            $PerformanceProfile = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Result.value))
+            Set-Content -Path $PerformanceProfilePath -Value $PerformanceProfile
+        }
+        else {
+            Write-Host "Could not download performance profile. Please ensure at least v1.4 of the Test Runner Service app is installed." -ForegroundColor DarkRed
+        }
     }
     else {
-        Write-Host "Could not download performance profile. Please ensure at least v1.4 of the Test Runner Service app is installed." -ForegroundColor DarkRed
+        $Body = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tes="urn:microsoft-dynamics-schemas/codeunit/TestRunner"><soapenv:Header/><soapenv:Body><tes:GetPerformanceProfile/></soapenv:Body></soapenv:Envelope>'
+        $Headers = (@{SOAPAction='Read'})
+        $Params = @{
+            Uri = $ServiceUrl
+            Method = 'Post'
+            ContentType = 'application/xml'
+            Body = $Body
+            Headers = $Headers
+            Credential  = $Credential
+        }
+
+        $Result = (Invoke-InvokeWebRequest $Params).Content
+        [xml]$ResultXml = $Result
+        $Result = $ResultXml.Envelope.Body.GetPerformanceProfile_Result.InnerText
+        
+        $PerformanceProfile = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Result))
+        Set-Content -Path $PerformanceProfilePath -Value $PerformanceProfile
     }
 }
 
