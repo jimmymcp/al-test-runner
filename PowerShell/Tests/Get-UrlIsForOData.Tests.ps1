1e98b9c5558b15c3d20e635c6b85c377c937a33e
James Pearson <james.pearson@totalenterprisesolutions.co.uk>
2024-04-16 11:57:15 +0100

    Update test syntax

--- PowerShell/Tests/Get-UrlIsForOData.Tests.ps1
+++ PowerShell/Tests/Get-UrlIsForOData.Tests.ps1
@@ -1,9 +1,9 @@
 Describe Get-UrlIsForOData {
     It 'should return true when the URL contains OData' {
-        Get-UrlIsForOData -Url 'http://localhost:7048/BC/ODataV4/TestRunner?company=CRONUS%20International%20Ltd.&tenant=default' | should be $true
+        Get-UrlIsForOData -Url 'http://localhost:7048/BC/ODataV4/TestRunner?company=CRONUS%20International%20Ltd.&tenant=default' | should -be $true
     }
 
     It 'should return false when the URL does not contain OData' {
-        Get-UrlIsForOData -Url 'http://localhost:7048/BC/WS/TestRunner?tenant=default' | should be $false
+        Get-UrlIsForOData -Url 'http://localhost:7048/BC/WS/TestRunner?tenant=default' | should -be $false
     }
 }
