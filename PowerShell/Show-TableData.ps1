815dfe02818fa8820d2bff919cf8decebb11139c
James Pearson <james.pearson@totalenterprisesolutions.co.uk>
2024-04-30 16:14:50 +0100

    Show Table Data doesn't work with SOAP endpoint Fixes #143

--- PowerShell/Show-TableData.ps1
+++ PowerShell/Show-TableData.ps1
@@ -9,16 +9,41 @@ function Show-TableData {
     $ServiceUrl = Get-ServiceUrl -Method GetTableIDFromName -LaunchConfig $LaunchConfig
     $Credential = Get-ALTestRunnerCredential
 
-    $Params = @{
-        Uri         = $ServiceUrl
-        Credential  = $Credential
-        Method      = 'Post' 
-        ContentType = 'application/json'
-        Body        = ("{`"tableName`": `"$TableName`"}")
+    if (Get-UrlIsForOData $ServiceUrl) {
+        $Params = @{
+            Uri         = $ServiceUrl
+            Credential  = $Credential
+            Method      = 'Post' 
+            ContentType = 'application/json'
+            Body        = ("{`"tableName`": `"$TableName`"}")
+        }
+        $Result = Invoke-InvokeWebRequest $Params
+        $Result = $Result | ConvertFrom-Json
+        $TableNo = $Result.value
     }
-    $Result = Invoke-InvokeWebRequest $Params
-    $Result = $Result | ConvertFrom-Json
-    $TableNo = $Result.value
+    else {
+        $Body = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tes="urn:microsoft-dynamics-schemas/codeunit/TestRunner">
+            <soapenv:Header/>
+                <soapenv:Body>
+                    <tes:GetTableIDFromName>
+                        <tes:tableName>' + $TableName + '</tes:tableName>
+                    </tes:GetTableIDFromName>
+                </soapenv:Body>
+            </soapenv:Envelope>'
+        $Headers = (@{SOAPAction='Read'})
+        $Params = @{
+            Uri = $ServiceUrl
+            Method = 'Post'
+            ContentType = 'application/xml'
+            Body = $Body
+            Headers = $Headers
+            Credential  = $Credential
+        }
+
+        $Result = (Invoke-InvokeWebRequest $Params).Content
+        [xml]$ResultXml = $Result
+        $TableNo = $ResultXml.Envelope.Body.GetTableIDFromName_Result.InnerText
+    }
     $Url = Get-WebClientUrl -LaunchConfig $LaunchConfig
     $Url += "&table=$TableNo"
     Start-Process $Url
